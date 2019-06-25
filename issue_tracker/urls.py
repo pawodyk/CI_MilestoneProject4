75f2ce6976fbf6256fdd7a077bc8f91696733bf0
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-25 19:13:55 +0000

    completed the view_ticket and edit_ticket functionality

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
-from .views import view_tracker, add_ticket, upvote_ticket
+from .views import view_tracker, add_ticket, upvote_ticket, view_ticket, edit_ticket
 
 
 urlpatterns = [
     url(r'^$', view_tracker, name='tracker'),
     url(r'^add_ticket/(?P<ticket_type>[A-Z]{1})/$', add_ticket,  name="add_ticket"),
     url(r'^upvote_ticket/(?P<ticket_id>\d+)/$', upvote_ticket, name="upvote_ticket"),
+    url(r'^view_ticket/(?P<ticket_id>\d+)/$', view_ticket, name="view_ticket"),
+    url(r'^edit_ticket/(?P<ticket_id>\d+)/$', edit_ticket, name="edit_ticket"),
+    
 ]
--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -26,6 +26,7 @@ def add_ticket(request, ticket_type):
                 request.session['ticket_id'] = new_ticket.id
                 return redirect('checkout') ## redirect to the payment
             
+            messages.success(request, "Ticket submitted sucessfully")
             return redirect(reverse('tracker'))
             
         
@@ -50,11 +51,36 @@ def upvote_ticket(request, ticket_id):
         request.session['ticket_id'] = ticket.id
         return redirect('checkout')
     
+    messages.success(request, "Ticket upvoted sucessfully")
     return redirect(reverse('tracker'))
     
-
-
-
-# def view_ticket(request, ticket_id):
-#     return render(reverse(tick))
+@login_required()
+def view_ticket(request, ticket_id):
     
+    is_owner = False
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+    
+    if ticket.created_by == request.user:
+        is_owner = True
+        
+        
+    return render(request, 'view_ticket.html', {"ticket": ticket, "is_owner": is_owner})
+    
+    
+@login_required()
+def edit_ticket(request, ticket_id):
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+    
+    if ticket.created_by == request.user:
+        
+        if request.method == 'POST':
+            form = NewTicketForm(request.POST, instance=ticket)
+            if form.is_valid():
+                form.save()
+                return redirect(reverse('tracker'))
+        else:
+            form = NewTicketForm(instance=ticket)
+            return render(request, 'edit_ticket.html', {'form': form})
+    
+    messages.error(request, "You need to be an owner to have permissions to edit this ticket")
+    return redirect(reverse('tracker'))
