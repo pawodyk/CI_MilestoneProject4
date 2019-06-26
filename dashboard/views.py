6408aaeed54a80fc998917912714aa4a5884c94c
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-26 17:03:19 +0000

    added functionality to the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from issue_tracker.models import Ticket, TICKET_STATUS
+from checkout.models import Order
+import datetime
 
 def display_dashboard(request):
     
-    return render(request, 'dashboard.html')
+    t_data =  Ticket.objects.all().values('name', 'ticket_type', 'status')
+    tickets = [t for t in t_data]
+    
+    for ticket in tickets:
+        ts = dict(TICKET_STATUS)
+        status = ts[ticket['status']]
+        
+        ticket['status'] = status
+    
+    
+    
+    o_data = Order.objects.all().values('date', 'amount')
+    orders = [{"Date" : str(o['date']), "Amount": float(o['amount'])} for o in o_data]
+    
+    print(orders)
+    
+    return render(request, 'dashboard.html', {'tickets': tickets, 'orders': orders})
--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -2,25 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 
+TICKET_TYPES = (
+    ('F', 'Feature'),
+    ('B', 'Bug'),
+)
+    
+TICKET_STATUS = (
+    ("0", "Completed"),
+    ("1", "In Progress"),
+    ("2", "Submitted"),
+    ("3", "On Hold"),
+    ("5", "Abandoned"),
+)
+
 class Ticket(models.Model):
-    
-    TICKET_TYPES = (
-        ('F', 'Feature'),
-        ('B', 'Bug'),
-    )
-    
-    TICKET_STATUS = (
-        ("0", "Completed"),
-        ("1", "In Progress"),
-        ("2", "On Hold"),
-        ("5", "Abandoned"),
-    )
     
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=1000)
     contibutions = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     ticket_type = models.CharField(max_length=1, choices=TICKET_TYPES)
-    status = models.CharField(max_length=1, default="1", choices=TICKET_STATUS)
+    status = models.CharField(max_length=1, default="2", choices=TICKET_STATUS)
 
     created_date = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name="author")
