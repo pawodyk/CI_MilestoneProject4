1cc498b89cc93444e6c8475567ed5ca13322fd8b
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-27 23:09:49 +0000

    completed tests for home app

--- home/tests.py
+++ home/tests.py
@@ -5,18 +5,17 @@ from .views import *
 from .apps import HomeConfig
 
 
-class TestViews(TestCase):
+class TestHomeViews(TestCase):
     """Testing Views"""
     
     def test_rendering_of_home_page(self):
         page = self.client.get("/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "index.html")
-        
 
-class TestApps(TestCase):
+     
+class TestHomeApps(TestCase):
     """Testing App"""
-
     def test_app_name(self):
         self.assertEqual("home", HomeConfig.name)
         self.assertEqual("home", apps.get_app_config("home").name)
