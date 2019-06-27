f92f4ed77b932f7369ca9fa93c131514a0f6bcb9
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-27 18:39:14 +0000

    added tests for the home page

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.apps import apps
 
-# Create your tests here.
+from .views import *
+from .apps import HomeConfig
+
+
+class TestViews(TestCase):
+    """Testing Views"""
+    
+    def test_rendering_of_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
+        
+
+class TestApps(TestCase):
+    """Testing App"""
+
+    def test_app_name(self):
+        self.assertEqual("home", HomeConfig.name)
+        self.assertEqual("home", apps.get_app_config("home").name)
