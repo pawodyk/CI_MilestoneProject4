22e129e71efbca0bef0feccd991f992da29bd22a
Pawel Wodyk <pawlosik@yahoo.com>
2019-06-28 12:33:40 +0000

    completed the tests for games app with 100% coverage

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.apps import apps
 
-# Create your tests here.
+from .apps import GameConfig
+from .views import *
+
+class TestGamesApps(TestCase):
+    """Testing App"""
+    def test_app_name(self):
+        self.assertEqual("games", GameConfig.name)
+        self.assertEqual("games", apps.get_app_config("games").name)
+
+class TestGamesViews(TestCase):
+    """Testing Views"""
+    
+    def test_rendering_of_home_page(self):
+        page = self.client.get("/games/brick_breaker/")
+        
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "brick_breaker.html")
+
+     
+
